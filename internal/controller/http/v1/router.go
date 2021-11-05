f4f00d0da0dc0310f6cec9e2a313af1b7f5b26d1
Jokius <djok69@gmail.com>
2021-11-05 17:08:32 +0300

    Add routers

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -5,41 +5,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-
-	// Swagger docs.
-	_ "github.com/evrone/go-clean-template/docs"
-	"github.com/evrone/go-clean-template/internal/usecase"
-	"github.com/evrone/go-clean-template/pkg/logger"
+	"github.com/jokius/news-telegram-bot/internal/usecase"
+	"github.com/jokius/news-telegram-bot/pkg/logger"
 )
 
-// NewRouter -.
-// Swagger spec:
-// @title       Go Clean Template API
-// @description Using a translation service as an example
-// @version     1.0
-// @host        localhost:8080
-// @BasePath    /v1
-func NewRouter(handler *gin.Engine, l logger.Interface, t usecase.Translation) {
+func NewRouter(handler *gin.Engine, l logger.InterfaceLogger, user usecase.User, token string) {
 	// Options
 	handler.Use(gin.Logger())
 	handler.Use(gin.Recovery())
 
-	// Swagger
-	swaggerHandler := ginSwagger.DisablingWrapHandler(swaggerFiles.Handler, "DISABLE_SWAGGER_HTTP_HANDLER")
-	handler.GET("/swagger/*any", swaggerHandler)
-
 	// K8s probe
 	handler.GET("/healthz", func(c *gin.Context) { c.Status(http.StatusOK) })
-
-	// Prometheus metrics
-	handler.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	// Routers
 	h := handler.Group("/v1")
 	{
-		newTranslationRoutes(h, t, l)
+		UserTelegramRoutes(h, user, token, l)
 	}
 }
