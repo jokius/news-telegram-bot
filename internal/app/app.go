e9bc1af61d219fe4a8694581592762c3878ca214
Jokius <djok69@gmail.com>
2021-11-05 17:09:26 +0300

    Add run application

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,50 +6,58 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/evrone/go-clean-template/config"
-	amqprpc "github.com/evrone/go-clean-template/internal/controller/amqp_rpc"
-	v1 "github.com/evrone/go-clean-template/internal/controller/http/v1"
-	"github.com/evrone/go-clean-template/internal/usecase"
-	"github.com/evrone/go-clean-template/internal/usecase/repo"
-	"github.com/evrone/go-clean-template/internal/usecase/webapi"
-	"github.com/evrone/go-clean-template/pkg/httpserver"
-	"github.com/evrone/go-clean-template/pkg/logger"
-	"github.com/evrone/go-clean-template/pkg/postgres"
-	"github.com/evrone/go-clean-template/pkg/rabbitmq/rmq_rpc/server"
+	"github.com/jokius/news-telegram-bot/config"
+	v1 "github.com/jokius/news-telegram-bot/internal/controller/http/v1"
+	"github.com/jokius/news-telegram-bot/internal/usecase"
+	"github.com/jokius/news-telegram-bot/internal/usecase/repo"
+	"github.com/jokius/news-telegram-bot/internal/usecase/service"
+	"github.com/jokius/news-telegram-bot/pkg/grabber"
+	"github.com/jokius/news-telegram-bot/pkg/httpclient"
+	"github.com/jokius/news-telegram-bot/pkg/httpserver"
+	"github.com/jokius/news-telegram-bot/pkg/logger"
+	"github.com/jokius/news-telegram-bot/pkg/postgres"
 )
 
 // Run creates objects via constructors.
 func Run(cfg *config.Config) {
 	l := logger.New(cfg.Log.Level)
 
+	l.Info(cfg.PG.URL)
 	// Repository
-	pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax))
+	pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax),
+		postgres.Debug(os.Getenv("DATABASE_DEBUG_INFO")))
+
 	if err != nil {
 		l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
 	}
-	defer pg.Close()
 
 	// Use case
-	translationUseCase := usecase.New(
-		repo.New(pg),
-		webapi.New(),
+	client := httpclient.NewClient()
+	source := service.NewVkSource(cfg.Vk.Token, client)
+	messenger := service.NewMessenger(cfg.Telegram.Token, cfg.Telegram.BaseURL, client, source, l)
+	userUseCase := usecase.NewUserUseCase(
+		repo.NewUserRepo(pg),
+		messenger,
+		source,
 	)
-
-	// RabbitMQ RPC Server
-	rmqRouter := amqprpc.NewRouter(translationUseCase)
-
-	rmqServer, err := server.New(cfg.RMQ.URL, cfg.RMQ.ServerExchange, rmqRouter, l)
-	if err != nil {
-		l.Fatal(fmt.Errorf("app - Run - rmqServer - server.New: %w", err))
-	}
 
 	// HTTP Server
 	handler := gin.New()
-	v1.NewRouter(handler, l, translationUseCase)
+	v1.NewRouter(handler, l, userUseCase, cfg.Telegram.Token)
 	httpServer := httpserver.New(handler, httpserver.Port(cfg.HTTP.Port))
+
+	// Grabbers server
+	var apiGrabbers []grabber.Grabber
+
+	sleepTime := time.Duration(cfg.Grabber.Sleep) * time.Second
+	groupRepo := repo.NewGroupRepo(pg)
+	messageRepo := repo.NewMessageRepo(pg)
+	vkGrabber := service.NewVkGrabber(sleepTime, source, messenger, groupRepo, messageRepo, l)
+	apiGrabbers = append(apiGrabbers, &vkGrabber)
+	grabbersServer := grabber.New(apiGrabbers)
 
 	// Waiting signal
 	interrupt := make(chan os.Signal, 1)
@@ -60,8 +68,6 @@ func Run(cfg *config.Config) {
 		l.Info("app - Run - signal: " + s.String())
 	case err = <-httpServer.Notify():
 		l.Error(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
-	case err = <-rmqServer.Notify():
-		l.Error(fmt.Errorf("app - Run - rmqServer.Notify: %w", err))
 	}
 
 	// Shutdown
@@ -70,8 +76,5 @@ func Run(cfg *config.Config) {
 		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
 	}
 
-	err = rmqServer.Shutdown()
-	if err != nil {
-		l.Error(fmt.Errorf("app - Run - rmqServer.Shutdown: %w", err))
-	}
+	grabbersServer.Shutdown()
 }
