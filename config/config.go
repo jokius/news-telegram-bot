ab6744f2f3e9006d91994bf1b48cdd3377d49976
Jokius <djok69@gmail.com>
2021-11-05 17:08:54 +0300

    Change config

--- config/config.go
+++ config/config.go
@@ -3,10 +3,13 @@ package config
 type (
 	// Config -.
 	Config struct {
-		App  `yaml:"app"`
-		HTTP `yaml:"http"`
-		Log  `yaml:"logger"`
-		PG   `yaml:"postgres"`
+		App      `yaml:"app"`
+		HTTP     `yaml:"http"`
+		Log      `yaml:"logger"`
+		PG       `yaml:"postgres"`
+		Telegram `yaml:"telegram"`
+		Vk       `yaml:"vk"`
+		Grabber  `yaml:"grabber"`
 	}
 
 	// App -.
@@ -29,5 +32,21 @@ type (
 	PG struct {
 		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
 		URL     string `env-required:"true"                 env:"PG_URL"`
+	}
+
+	// Telegram -.
+	Telegram struct {
+		BaseURL string `env-required:"true" yaml:"base_url" env:"TELEGRAM_BASE_URL"`
+		Token   string `env-required:"true" env:"TELEGRAM_TOKEN"`
+	}
+
+	// Vk -.
+	Vk struct {
+		Token string `env-required:"true" env:"VK_TOKEN"`
+	}
+
+	// Grabber -.
+	Grabber struct {
+		Sleep int64 `env-required:"true" yaml:"sleep" env:"GRABBER_SLEEP"`
 	}
 )
